88cd6a8d38fad4ccb1edf43bf1ca136f303d2bea
Karpovich Vladimir <dev.karpovich@gmail.com>
2016-12-26 16:08:21 +0500

    fixed untracked files

--- vendor/rachidlaasri/laravel-installer/src/Views/finished.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('vendor.installer.layouts.master')
-
-@section('title', trans('messages.install.final.title'))
-@section('container')
-    <p class="paragraph">{{ session('message')['message'] }}</p>
-    <div class="buttons">
-        <a href="{{ url() }}" class="button">{{ trans('messages.install.final.exit') }}</a>
-    </div>
-@stop
