34c89672861984d64af6654e3c4b64654a64b43c
Karpovich Vladimir <dev.karpovich@gmail.com>
2016-12-26 21:51:13 +0500

    fixed untracked files

--- demo-site/vendor/rachidlaasri/laravel-installer/src/Views/welcome.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('vendor.installer.layouts.master')
-
-@section('title', trans('messages.install.welcome.title'))
-@section('container')
-    <p class="paragraph">{{ trans('messages.install.welcome.message') }}</p>
-    <div class="buttons">
-        <a href="{{ route('LaravelInstaller::environment') }}" class="button">{{ trans('messages.install.next') }}</a>
-    </div>
-@stop
