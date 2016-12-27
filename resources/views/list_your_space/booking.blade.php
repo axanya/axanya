4e9983dc99267929521d6bf456758a59f875980c
Karpovich Vladimir <dev.karpovich@gmail.com>
2016-12-27 15:34:26 +0500

    hide guest_book_instantly

--- resources/views/list_your_space/booking.blade.php
+++ resources/views/list_your_space/booking.blade.php
@@ -49,7 +49,7 @@
 </div>
 </div>
 <hr class="space-2">
-<div style="float:none;" class="col-md-8 space-2">
+<div style="float:none; display: none;" class="col-md-8 space-2">
 
 <div style="" class="panel ">
 <div style="float:left; padding-top: 10px;">
